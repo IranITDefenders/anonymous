77c76fca75b9392764ffa0ac5bf9b6c9fea59a74
me-dira <mediraworkm@gmail.com>
2023-01-23 11:10:42 +0330

    feat: added controllers directory and bootstrap

--- src/core/entity/Anonymous.ts
+++ src/core/entity/Anonymous.ts
@@ -2,7 +2,6 @@ import {Context} from 'telegraf';
 import {Entity} from '@interface/abstract/Entity';
 import {listener} from '@shared/decorators/listener.decorator';
 import {TelegrafProvider} from '@core/entity/FakeTelegramEntity';
-import {forceReply} from 'telegraf/typings/markup';
 
 export class Anonymous extends Entity {
   private _fakeTelegram: TelegrafProvider;
@@ -15,21 +14,11 @@ export class Anonymous extends Entity {
 
   @listener('sticker')
   onSticker(ctx: Context) {
-    console.log({
-      type: 'sticker',
-      from: ctx.from,
-    });
-
     ctx.reply('You send an sticker');
   }
 
   @listener('message')
   onMessage(ctx: Context) {
-    console.log({
-      type: 'message',
-      from: ctx.from,
-    });
-
     ctx.reply(`${ctx.from?.username}, You send a message :)`);
   }
 
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,6 @@ importEnvs();
 
 import 'reflect-metadata';
 import 'module-alias/register';
+import bootstrap from './bootstrap';
 
-import {Anonymous} from '@core/entity/Anonymous';
-
-// /**
-//  * @TODO: Clean the code and setup tsyringe container
-//  * @TODO: Make sure sure specific classes are singleton
-//  * @TODO: Ability to chain the processes.
-//  * @TODO: Make changes on database.
-//  * @TODO: Add sink package to add subscribers to it.
-//  */
-
-const anonymousBot = new Anonymous();
-anonymousBot.init();
+bootstrap();
